283d5d7d438072ae8207f2c8abc15ec2c6e2e28f
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-26 18:25:03 +1000

    Add a .NET Core unit test

---
+++ src/Trezor.Net.NETCoreUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trezor.Net.NETCoreUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
