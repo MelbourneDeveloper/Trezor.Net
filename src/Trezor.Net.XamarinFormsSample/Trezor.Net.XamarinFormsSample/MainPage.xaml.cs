880ae5d45953f9f810d7360778a42715d461e94d
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-01 15:12:28 +1000

    UWP Working

--- src/Trezor.Net.XamarinFormsSample/Trezor.Net.XamarinFormsSample/MainPage.xaml.cs
+++ src/Trezor.Net.XamarinFormsSample/Trezor.Net.XamarinFormsSample/MainPage.xaml.cs
@@ -12,11 +12,12 @@ namespace Trezor.Net.XamarinFormsSample
 
         internal async Task DisplayAddressAsync()
         {
-            Device.BeginInvokeOnMainThread(async () => 
+            Device.BeginInvokeOnMainThread(async () =>
             {
                 var app = Application.Current as App;
                 await app.TrezorManager.InitializeAsync();
-                TheLabel.Text = await app.TrezorManager.GetAddressAsync("BTC", 0, false, 0, false, AddressType.Bitcoin);
+                TheLabel.Text = $"First Bitcoin Address: {await app.TrezorManager.GetAddressAsync("BTC", 0, false, 0, false, AddressType.Bitcoin)}";
+                TheActivityIndicator.IsRunning = false;
             });
         }
     }
