7fbac6748a7400ded4271d4bfcf97e684ec3aae1
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-10-06 10:35:13 +1000

    Add a method to get address type from the coin type.

--- src/Trezor.Net/Manager/DefaultCoinUtility.cs
+++ src/Trezor.Net/Manager/DefaultCoinUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Trezor.Net
 {
@@ -18,6 +19,19 @@ namespace Trezor.Net
                     return new CoinInfo("Bcash", AddressType.Bitcoin);
                 default:
                     throw new NotImplementedException($"The coin number {coinNumber} has not been filled in for {nameof(DefaultCoinUtility)}. Please create a class that implements ICoinUtility, or call an overload that specifies coin information.");
+            }
+        }
+
+        public static IEnumerable<uint> GetCoinTypesByAddressType(AddressType addressType)
+        {
+            switch (addressType)
+            {
+                case AddressType.Bitcoin:
+                    return new List<uint> { 0, 145, 156 };
+                case AddressType.Ethereum:
+                    return new List<uint> { 60, 61 };
+                default:
+                    throw new NotImplementedException();
             }
         }
     }
