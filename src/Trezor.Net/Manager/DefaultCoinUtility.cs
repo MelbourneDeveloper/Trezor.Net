ecafe7af1cad5103c65f14b77ce288d7c659beea
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-10-14 19:14:42 +1100

    Fix default coin utility

--- src/Trezor.Net/Manager/DefaultCoinUtility.cs
+++ src/Trezor.Net/Manager/DefaultCoinUtility.cs
@@ -1,38 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Xml.Serialization;
 
 namespace Trezor.Net
 {
     public class DefaultCoinUtility : ICoinUtility
     {
+        public List<CoinInfo> Coins { get; }
+
         public DefaultCoinUtility()
         {
-            //var assembly = typeof(TrezorManager).GetTypeInfo().Assembly;
-            //var stream = assembly.GetManifestResourceStream("Trezor.Net.Resources.Coins.json");
-            //string json;
-            //using (var reader = new StreamReader(stream))
-            //{
-            //    json = reader.ReadToEnd();
-            //}
-            //return json;
+            var assembly = typeof(TrezorManager).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Trezor.Net.Resources.Coins.xml");
+            string xml;
+            using (var reader = new StreamReader(stream))
+            {
+                xml = reader.ReadToEnd();
+            }
+            Coins = DeserialiseObject<List<CoinInfo>>(xml);
+            Coins.Add(new CoinInfo("Ethereum", AddressType.Ethereum, false, 60));
+            Coins.Add(new CoinInfo("Ethereum Classic", AddressType.Ethereum, false, 61));
         }
 
         public CoinInfo GetCoinInfo(uint coinNumber)
         {
-            switch (coinNumber)
+            var coinInfo = Coins.FirstOrDefault(c => c.CoinType == coinNumber);
+            if (coinInfo != null)
             {
-                case 0:
-                    return new CoinInfo(null, AddressType.Bitcoin, true, coinNumber);
-                case 60:
-                    return new CoinInfo(null, AddressType.Ethereum, false, coinNumber);
-                case 61:
-                    return new CoinInfo(null, AddressType.Ethereum, false, coinNumber);
-                case 145:
-                    return new CoinInfo("Bcash", AddressType.Bitcoin, true, coinNumber);
-                default:
-                    throw new NotImplementedException($"The coin number {coinNumber} has not been filled in for {nameof(DefaultCoinUtility)}. Please create a class that implements ICoinUtility, or call an overload that specifies coin information.");
+                return coinInfo;
             }
+
+            throw new NotImplementedException($"The coin number {coinNumber} has not been filled in for {nameof(DefaultCoinUtility)}. Please create a class that implements ICoinUtility, or call an overload that specifies coin information.");
+        }
+
+        public static SerialiseType DeserialiseObject<SerialiseType>(string objectXml)
+        {
+            var serializer = new XmlSerializer(typeof(SerialiseType));
+            var sr = new StringReader(objectXml);
+            var retVal = serializer.Deserialize(sr);
+            return (SerialiseType)retVal;
         }
     }
 }
