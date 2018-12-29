3d759bdaafa2ec09ebcff32f94c230483c986682
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-29 19:06:16 +1100

    MOre compile

--- src/Trezor.Net.UnitTest/WindowsTrezorOneOldTests.cs
+++ src/Trezor.Net.UnitTest/WindowsTrezorOneOldTests.cs
@@ -1,8 +1,10 @@
 ﻿using Device.Net;
 using Hid.Net.Windows;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Usb.Net.Windows;
 
 namespace Trezor.Net
 {
@@ -11,21 +13,24 @@ namespace Trezor.Net
     {
         protected override async Task<IDevice> Connect()
         {
+            //Register the factory for creating Usb devices. This only needs to be done once.
+            WindowsUsbDeviceFactory.Register();
             WindowsHidDeviceFactory.Register();
-            //WindowsUsbDeviceFactory.Register();
 
-            var devices = await DeviceManager.Current.GetDevices(TrezorManager.DeviceDefinitions);
-            var trezorDevice = devices.FirstOrDefault();
+            //Note: other custom device types could be added here
 
-            if (trezorDevice == null)
-            {
-                throw new System.Exception("No Trezor was connected");
-            }
+            //Define the types of devices to search for. This particular device can be connected to via USB, or Hid
+            var deviceDefinitions = new List<DeviceDefinition>
+                {
+                    new DeviceDefinition{ DeviceType= DeviceType.Hid, VendorId= 0x534C, ProductId=0x0001, Label="Trezor One Firmware 1.6.x" },
+                    new DeviceDefinition{ DeviceType= DeviceType.Usb, VendorId= 0x1209, ProductId=0x53C1, ReadBufferSize=64, WriteBufferSize=64, Label="Trezor One Firmware 1.7.x" },
+                    new DeviceDefinition{ DeviceType= DeviceType.Usb, VendorId= 0x1209, ProductId=0x53C0, ReadBufferSize=64, WriteBufferSize=64, Label="Model T" }
+                };
 
-            await trezorDevice.InitializeAsync();
+            //Get the first available device and connect to it
+            var devices = await DeviceManager.Current.GetDevices(deviceDefinitions);
+            return devices.FirstOrDefault();
 
-   
-            return trezorDevice;
         }
     }
 }
