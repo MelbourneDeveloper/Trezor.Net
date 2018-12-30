82495c5ab4085c9c4c6a19578710ac1273a99ce1
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-30 12:07:40 +1100

    Split off a new Unit Test project for LibUsb

---
+++ src/Trezor.Net.LibUsbUnitTest/WindowsTestBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Trezor.Net
+{
+    public abstract class WindowsTestBase : UnitTestBase
+    {
+        #region Platform Specific Overrides
+        protected string GetExecutingAssemblyDirectoryPath()
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var executingAssemblyDirectoryPath = Path.GetDirectoryName(uri.Path);
+            return executingAssemblyDirectoryPath;
+        }
+
+        protected override async Task<string> GetPin()
+        {
+            var passwordExePath = Path.Combine(GetExecutingAssemblyDirectoryPath(), "Misc", "GetPassword.exe");
+            if (!File.Exists(passwordExePath))
+            {
+                throw new Exception($"The pin exe doesn't exist at passwordExePath {passwordExePath}");
+            }
+
+            var process = Process.Start(passwordExePath);
+            process.WaitForExit();
+            await Task.Delay(100);
+            var pin = File.ReadAllText(Path.Combine(GetExecutingAssemblyDirectoryPath(), "pin.txt"));
+            return pin;
+        }
+        #endregion
+    }
+}
---
+++ src/Trezor.Net.LibUsbUnitTest/WindowsTests.cs
@@ -0,0 +1,36 @@
+﻿using Device.Net;
+using Hid.Net.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Usb.Net.Windows;
+
+namespace Trezor.Net
+{
+    [TestClass]
+    public class WindowsTests : WindowsTestBase
+    {
+        protected override async Task<IDevice> Connect()
+        {
+            //This only needs to be done once.
+            //Register the factory for creating Usb devices. Trezor One Firmware 1.7.x / Trezor Model T
+            WindowsUsbDeviceFactory.Register();
+            //Register the factory for creating Hid devices. Trezor One Firmware 1.6.x
+            WindowsHidDeviceFactory.Register();
+
+            //Get the first available device and connect to it
+            var devices = await DeviceManager.Current.GetDevices(TrezorManager.DeviceDefinitions);
+            var trezorDevice = devices.FirstOrDefault();
+
+            if (trezorDevice == null)
+            {
+                throw new Exception("No trezor connected");
+            }
+
+            await trezorDevice.InitializeAsync();
+
+            return trezorDevice;
+        }
+    }
+}
