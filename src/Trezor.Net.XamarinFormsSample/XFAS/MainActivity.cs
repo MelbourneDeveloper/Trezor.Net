f46e6876f06663afce42b7b3f94c88bdd3d894eb
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-01-15 21:00:14 +1100

    Android works!

--- src/Trezor.Net.XamarinFormsSample/XFAS/MainActivity.cs
+++ src/Trezor.Net.XamarinFormsSample/XFAS/MainActivity.cs
@@ -4,9 +4,11 @@ using Android.Content;
 using Android.Content.PM;
 using Android.Hardware.Usb;
 using Android.OS;
+using Android.Widget;
 using Device.Net;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Usb.Net.Android;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
@@ -21,27 +23,39 @@ namespace Trezor.Net.XamarinFormsSample.Droid
         private AndroidUsbDevice _TrezorUsbDevice;
         #endregion
 
-        protected async override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
-            //TODO: Error handling. If something goes wrong here, or the device is not connected the whole app will crash
-
-            var usbManager = GetSystemService(UsbService) as UsbManager;
-            if (usbManager == null) throw new Exception("UsbManager is null");
-
-            //Register the factory for creating Usb devices. This only needs to be done once.
-            AndroidUsbDeviceFactory.Register(usbManager, base.ApplicationContext);
-
-
-            var devices = await DeviceManager.Current.GetDevices(TrezorManager.DeviceDefinitions);
-            _TrezorUsbDevice = (AndroidUsbDevice)devices.FirstOrDefault();
-
-
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
             Forms.Init(this, savedInstanceState);
-            LoadApplication(new App(_TrezorUsbDevice));
+            Go();
+        }
+
+        private async Task Go()
+        {
+            try
+            {
+                //TODO: Error handling. If something goes wrong here, or the device is not connected the whole app will crash
+
+                var usbManager = GetSystemService(UsbService) as UsbManager;
+                if (usbManager == null) throw new Exception("UsbManager is null");
+
+                //Register the factory for creating Usb devices. This only needs to be done once.
+                AndroidUsbDeviceFactory.Register(usbManager, base.ApplicationContext);
+
+
+                var devices = await DeviceManager.Current.GetDevices(TrezorManager.DeviceDefinitions);
+                _TrezorUsbDevice = (AndroidUsbDevice)devices.FirstOrDefault();
+
+
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
+
+                LoadApplication(new App(_TrezorUsbDevice));
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(ApplicationContext, ex.ToString(), ToastLength.Long).Show();
+            }
         }
     }
 }
