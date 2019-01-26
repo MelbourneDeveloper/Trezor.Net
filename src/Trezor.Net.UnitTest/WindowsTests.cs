d85912ed242beec190787278085a031c3e055324
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-01-26 19:14:16 +1100

    Cleanup

--- src/Trezor.Net.UnitTest/WindowsTests.cs
+++ src/Trezor.Net.UnitTest/WindowsTests.cs
@@ -1,7 +1,6 @@
 ﻿using Hid.Net.Windows;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
-using Trezor.Net.Manager;
 using Usb.Net.Windows;
 
 namespace Trezor.Net
@@ -15,6 +14,7 @@ namespace Trezor.Net
             //This only needs to be done once.
             //Register the factory for creating Usb devices. Trezor One Firmware 1.7.x / Trezor Model T
             WindowsUsbDeviceFactory.Register();
+
             //Register the factory for creating Hid devices. Trezor One Firmware 1.6.x
             WindowsHidDeviceFactory.Register();
 
