91f1ae2d94bd93ae993f51071721be254f1fe22a
Christian <anemail@nothing.com>
2019-04-22 12:30:36 +1000

    Remove the Csharp NuGet dependency

--- src/Trezor.Net/Manager/FailureException.cs
+++ src/Trezor.Net/Manager/FailureException.cs
@@ -7,9 +7,15 @@ namespace Trezor.Net
     {
         public T Failure { get; }
 
-        public FailureException(string message, T failure) : base($"{message}\r\n{(failure as dynamic)?.Message}")
+        public FailureException(string message, T failure) : base($"{message}\r\n{GetFailureMessage(failure)}")
         {
             Failure = failure;
+        }
+
+        private static string GetFailureMessage(object failure)
+        {
+            var messageProperty = failure.GetType().GetProperty("Message");
+            return messageProperty != null ? messageProperty.GetValue(failure, null) as string : null;
         }
     }
 }
