2ccfe255dda5a5c751d011ae3bcd270c4279aed8
Christian Findlay <a@a.com>
2019-08-03 13:34:11 +1000

    Upgrade Device.Net

--- src/Trezor.Net.UnitTest/WindowsTests.cs
+++ src/Trezor.Net.UnitTest/WindowsTests.cs
@@ -1,4 +1,5 @@
-﻿using Hid.Net.Windows;
+﻿using Device.Net;
+using Hid.Net.Windows;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using Usb.Net.Windows;
@@ -13,10 +14,10 @@ namespace Trezor.Net
         {
             //This only needs to be done once.
             //Register the factory for creating Usb devices. Trezor One Firmware 1.7.x / Trezor Model T
-            WindowsUsbDeviceFactory.Register();
+            WindowsUsbDeviceFactory.Register(new DebugLogger(), new DebugTracer());
 
             //Register the factory for creating Hid devices. Trezor One Firmware 1.6.x
-            WindowsHidDeviceFactory.Register();
+            WindowsHidDeviceFactory.Register(new DebugLogger(), new DebugTracer());
 
             return base.ConnectAsync();
         }
