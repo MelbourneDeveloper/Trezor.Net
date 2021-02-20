29c169a8d721697f4485baf230ca4416336d33b6
Christian Findlay <email@email.email>
2021-02-21 08:32:41 +1100

    Fix some code rules

--- src/Trezor.Net.XamarinFormsSample/Trezor.Net.XamarinFormsSample.UWP/App.xaml.cs
+++ src/Trezor.Net.XamarinFormsSample/Trezor.Net.XamarinFormsSample.UWP/App.xaml.cs
@@ -18,8 +18,8 @@ namespace Trezor.Net.XamarinFormsSample.UWP
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
+            Suspending += OnSuspending;
         }
 
         /// <summary>
@@ -29,13 +29,9 @@ namespace Trezor.Net.XamarinFormsSample.UWP
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-
-
-            var rootFrame = Window.Current.Content as Frame;
-
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
-            if (rootFrame == null)
+            if (Window.Current.Content is not Frame rootFrame)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
@@ -58,7 +54,7 @@ namespace Trezor.Net.XamarinFormsSample.UWP
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                _ = rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
